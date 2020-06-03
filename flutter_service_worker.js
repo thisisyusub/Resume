e7c235d090b199296566b1f837cd77c38b6af5ec
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-06-03 14:10:33 +0400

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,14 +5,14 @@ const RESOURCES = {
 "assets/assets/profile.jpg": "5665ca73554fff8242bfb0049791ddc7",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "7794d4d4e4ab00162177430a3ae4a191",
+"assets/LICENSE": "3df0c4f0d254153bb88a99cdd55072fd",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "42a31b3a9c0a4155c2ecdf15975e2142",
 "/": "42a31b3a9c0a4155c2ecdf15975e2142",
-"main.dart.js": "7c404abe9693436c542c90ca054053fc",
+"main.dart.js": "9e917d18699c5329c5385998048abe3d",
 "manifest.json": "d48f459376fc7cbf2207f267314b7ef3"
 };
 
